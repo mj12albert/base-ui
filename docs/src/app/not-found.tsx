964f399269247c64e312d5dd224272d9970612ef
Albert Yu <albert@albertyu.co>
2024-12-20 20:44:00 +0800

    Link h2 and h3 in docs

--- docs/src/app/not-found.tsx
+++ docs/src/app/not-found.tsx
@@ -21,7 +21,7 @@ export default function NotFound() {
               className="-m-1 inline-flex items-center gap-1 p-1"
               href="/react/components/accordion"
             >
-              Documentation <ArrowRightIcon />
+              Documentation <ArrowRightIcon aria-hidden />
             </Link>
 
             <Link className="-m-1 p-1" href="https://github.com/mui/base-ui">
